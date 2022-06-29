64b821b918fb0f4269ef9096ac8c9a33ce6f0628
Luca But <luca.but@stud.itsaltoadriatico.it>
2022-06-29 13:00:12 +0200

    gen-pass

--- timbratrice2/src/pages/admin.js
+++ timbratrice2/src/pages/admin.js
@@ -3,15 +3,9 @@ import './admin.css';
 import axios from "axios";
 import swal from "sweetalert";
 import { useNavigate } from 'react-router-dom';
+import { numbers, uppercase, lowercase, symbols } from "./character";
 
 function Admin() {
-
-    const [password, setPassword] = useState('')
-    const [passwordLenght, setPasswordLenght] = useState(20)
-    const [includeUppercase, setIncludeUppercase] = useState(false)
-    const [includeLowercase, setIncludeLowercase] = useState(false)
-    const [includeNumbers, setIncludeNumbers] = useState(false)
-    const [includeSymbols, setIncludeSymbols] = useState(false)
 
     const history = useNavigate();
 
@@ -48,6 +42,64 @@ function Admin() {
         });
     }
 
+    const [passwordG, setPassword] = useState('')
+    const [passwordLenght, setPasswordLenght] = useState(20)
+    const [includeUppercase, setIncludeUppercase] = useState(false)
+    const [includeLowercase, setIncludeLowercase] = useState(false)
+    const [includeNumbers, setIncludeNumbers] = useState(false)
+    const [includeSymbols, setIncludeSymbols] = useState(false)
+
+    const handleGeneratePassword = (e) => {
+        let characterList = ''
+        
+        if(includeLowercase){
+            characterList = characterList + lowercase
+        }
+
+        if(includeUppercase){
+            characterList = characterList + uppercase
+        }
+
+        if(includeNumbers){
+            characterList = characterList + numbers
+        }
+
+        if(includeSymbols){
+            characterList = characterList + symbols
+        }
+
+        setPassword(createPassword(characterList))
+    }
+
+    const createPassword = (characterList) => {
+        var passwordG = ''
+        const characterListLenght = characterList.length
+
+        for(var i = 0; i < passwordLenght; i++){
+            const characterIndex = Math.round(Math.random() * characterListLenght)
+            passwordG = passwordG + characterList.charAt(characterIndex)
+        }
+        return passwordG
+    }
+
+    const copyToClipboard = () => {
+        const newTextArea = document.createElement('textarea')
+        newTextArea.innerText = passwordG
+        document.body.appendChild(newTextArea)
+        newTextArea.select()
+        document.execCommand('copy')
+        newTextArea.remove()
+        swal({
+            icon: 'success',
+            text: 'copy success'
+        })
+    }
+
+    const handleCopyPassword = (e) => {
+        copyToClipboard()
+    }
+
+
     return (
         <div>
             <form onSubmit={registerSubmit} className="form-user">
@@ -74,47 +126,47 @@ function Admin() {
                 <input className='iU' type="password" name="password" onChange={handleInput} value={registerInput.password} />
                 <span>{registerInput.error_list.password}</span>
             </label>
+            <center><button className='bsub' type='submit'>Submit</button></center>
                 <center>
                     <div>
                         <div className="generator">
                             <h4 className="generator-header">Password Generator</h4>
                             <div className="generator_password">
-                                <h3>{password}</h3>
-                                <button className="copy_btn">
+                                <h3>{passwordG}</h3>
+                                <button onClick={handleCopyPassword} className="copy_btn">
                                     <i className="far fa-clipboard"></i>
                                 </button>
                             </div>
 
                             <div className="form-group">
                             <label htmlFor="password-strenght">Password lenght</label>
-                            <input type="number" id="password-strenght" name="password-strenght" max="20" min="8" />
+                            <input defaultValue={passwordLenght} onChange={(e) => setPasswordLenght(e.target.value)} type="number" id="password-strenght" name="password-strenght" max="20" min="8" />
                             </div>
 
                             <div className="form-group">
                             <label htmlFor="uppercase-letters">Include Uppercase letters</label>
-                            <input type="checkbox" id="uppercase-letters" name="uppercase-letters" max="20" min="8" />
+                            <input checked={includeUppercase} onChange={(e) => setIncludeUppercase(e.target.value)} type="checkbox" id="uppercase-letters" name="uppercase-letters" max="20" min="8" />
                             </div>
 
                             <div className="form-group">
                             <label htmlFor="lowercase-letters">Include Lowercase letters</label>
-                            <input type="checkbox" id="lowercase-letters" name="lowercase-letters" max="20" min="8" />
+                            <input checked={includeLowercase} onChange={(e) => setIncludeLowercase(e.target.value)} type="checkbox" id="lowercase-letters" name="lowercase-letters" max="20" min="8" />
                             </div>
 
                             <div className="form-group">
                             <label htmlFor="include-numbers">Include Numbers</label>
-                            <input type="checkbox" id="include-numbers" name="include-numbers" max="20" min="8" />
+                            <input checked={includeNumbers} onChange={(e) => setIncludeNumbers(e.target.value)} type="checkbox" id="include-numbers" name="include-numbers" max="20" min="8" />
                             </div>
 
                             <div className="form-group">
                             <label htmlFor="include-symbols">Include Symbols</label>
-                            <input type="checkbox" id="include-symbols" name="include-symbols" max="20" min="8" />
+                            <input checked={includeSymbols} onChange={(e) => setIncludeSymbols(e.target.value)} type="checkbox" id="include-symbols" name="include-symbols" max="20" min="8" />
                             </div>
 
-                            <button className="generator-btn">Generate password</button>
+                            <button onClick={handleGeneratePassword} className="generator-btn">Generate password</button>
                         </div>
                     </div>
                 </center>
-                    <center><button className='bsub' type='submit'>Submit</button></center>
             </form>
         </div>
     );
